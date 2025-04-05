6c27706ceb822f249e0a04003e966da607611525
Zonix <zonix300@gmail.com>
2025-04-05 23:16:26 +0200

    feat: added status effects enum

---
+++ src/main/java/com/zonix/dndapp/entity/StatusEffect.java
@@ -0,0 +1,17 @@
+package com.zonix.dndapp.entity;
+
+public enum StatusEffect {
+    BLINDED,
+    CHARMED,
+    DEAFENED,
+    FRIGHTENED,
+    GRAPPLED,
+    INCAPACITATED,
+    INVISIBLE,
+    PARALYZED,
+    PETRIFIED,
+    POISONED,
+    PRONE,
+    RESTRAINED,
+    UNCONSCIOUS,
+}
